03fdd8c772c44f60c6670925befc5481557a600f
Herbert <herbert.he0229@gmail.com>
2022-04-18 13:32:51 +0800

    fix :bug: : 修复样式错误

--- src/components/Introduce/Introduce.tsx
+++ src/components/Introduce/Introduce.tsx
@@ -24,29 +24,29 @@ const Introduce: FC<IIntroduceProps> = ({ type, setType }) => {
 
     return <div id="cordle-intro" className="cordle-intro">
         <div onClick={() => closeIntro()} className="absolute right-5 top-5">X</div>
-        <div>
+        <div className="flex flex-col justify-center items-center">
             <p className="cordle-intro-title">哈兜</p>
             <p className="cordle-intro-text">哈萨克语版 Wordle</p>
         </div>
 
-        <div>
+        <div className="flex flex-col justify-center items-center">
             <p className="cordle-intro-subtitle">游戏规则</p>
             <p className="cordle-intro-text">随着单词的长度你有不同次数的尝试机会猜一个 <b className="cordle-intro-point">哈语单词</b>。</p>
             <p className="cordle-intro-text">每次猜测后，格子中的颜色将会标识其与正确答案的区别。</p>
         </div>
 
         {/* 示例1 */}
-        <div>
+        <div className="flex flex-col justify-center items-center">
             <CharBox chars={e1} />
             <p className="cordle-intro-text">上面的例子中，<span className="text-[#de7525]">橙色</span>代表当前字母在单词中，但不在正确的位置上。<span className="text-[#1d9c9c]">绿色</span>代表在正确的位置上。</p>
         </div>
         {/* 示例2 */}
-        <div>
+        <div className="flex flex-col justify-center items-center">
             <CharBox chars={e2} />
             <p className="cordle-intro-text">上面的例子中，<span className="text-[#5d6673]">灰色</span>代表当前字母不在单词中。</p>
         </div>
 
-        <div>
+        <div className="flex flex-col justify-center items-center">
             <CharBox chars={e3} />
             <p className="cordle-intro-text">卡片全部为<span className="text-[#1d9c9c]">绿色</span>则恭喜你答对了！</p>
         </div>
